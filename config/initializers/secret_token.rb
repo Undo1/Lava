f02b9fbce1a991ddd246deb210f8230a532aac57
Undo1 <parker@erwaysoftware.com>
2014-03-18 10:14:28 -0600

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoltenPlagiarism::Application.config.secret_token = 'ec71b68149693fba0b4991e46ba218e01d993930c86e8d649fd11d007d5de6223f2d401d72a3c90e827160dfda881fe823bed20e0cd46d0d61a5f54d756d1f68'
