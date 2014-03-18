c61d2aa2fdd24a708d072fe9f61c511229ac2313
Undo1 <parker@erwaysoftware.com>
2014-03-18 10:50:55 -0600

    play with Google searching

--- detector.py
+++ detector.py
@@ -1,3 +1,9 @@
 import re
 import mechanize
 
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_equiv(False) 
+br.addheaders = [('User-agent', 'Firefox')]
+
+print br.open("https://www.google.com/search?q=this+is+a+test").read()
