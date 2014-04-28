584e0d799a732b1982c87b8cac0430b0729382f7
Undo1 <parker@erwaysoftware.com>
2014-04-28 10:54:52 -0600

    encode query

--- detector.py
+++ detector.py
@@ -1,6 +1,7 @@
 import re
 import mechanize
 import sys
+import urllib
 from BeautifulSoup import BeautifulSoup
 
 br = mechanize.Browser()
@@ -8,7 +9,7 @@ br.set_handle_robots(False)
 br.set_handle_equiv(False) 
 br.addheaders = [('User-agent', 'Firefox')]
 
-html = br.open('https://www.google.com/search?q="' + sys.argv[1] + '"').read()
+html = br.open('https://www.google.com/search?q="' + urllib.quote(sys.argv[1]) + '"').read()
 
 soup = BeautifulSoup(html)
 
