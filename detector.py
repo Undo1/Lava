df812d97b2f0bd34681acb31d471a9558f53b196
Undo1 <parker@erwaysoftware.com>
2015-01-03 14:22:05 -0700

    rough outline

--- detector.py
+++ detector.py
@@ -9,11 +9,24 @@ br.set_handle_robots(False)
 br.set_handle_equiv(False) 
 br.addheaders = [('User-agent', 'Firefox')]
 
+print "---------"
+print "-- Searching for: " + sys.argv[1]
+print "---------"
+
 html = br.open('https://www.google.com/search?q="' + urllib.quote(sys.argv[1]) + '"').read()
 
 soup = BeautifulSoup(html)
 
+if "No results found for" in soup.text:
+	print "-- No Results Found"
+	sys.exit("")
+
 cites = soup.findAll('cite')
 
+p=re.compile("\/url\?q=(.*)&sa")
+
 for elem in cites:
-	print elem.text
+	try:
+		print p.search(elem.parent.parent.parent.find('h3').find('a').get('href')).groups()[0]
+	except AttributeError:
+		print "meh"
---
+++ getposts.py
@@ -0,0 +1,15 @@
+import requests
+import random
+import re
+
+url = 'http://api.stackexchange.com/2.2/answers?pagesize=1&order=desc&sort=activity&site=stackoverflow&filter=!)Q2AgVryMp_tSg8sztC*RY8i'
+
+response = requests.get(url).json()
+
+test = response["items"][0]["body"]
+
+try:
+	p=re.compile("([\w*\s]{50,})")
+	print random.choice(p.search(test).groups())
+except:
+	print "meh"
